00622a39d9682c598e9f06a70ce4506c13774090
Luis Martinez <jarlmtz@localhost.localdomain>
2020-03-30 16:41:58 +0000

    Modifiqué un poco el modal que se muestra cuando el jugador gana, ahora dice "Encontraste x pares en y intentos"

--- src/components/PlayerWonModal/index.js
+++ src/components/PlayerWonModal/index.js
@@ -1,14 +1,37 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+import { startNewGame } from '../../redux/actions'
 
 import './index.scss'
-export default ({ newGame, tries }) => {
+
+const WonModal = (props) => {
+
+    const { rickCharacters, numberOfPairsSelected,
+            startNewGame, numOfPairsOnDeck} = props
+
     return (
-        <div className='wonModalContainer'>
+        <div className='wonModalContainer' >
             <div className='wonModal'>
-                <h1>Ganaste</h1>
-                <p>¡Lo lograste en {tries} intentos!</p>
-                <p onClick={newGame}>Volver a jugar</p>
+                <h1>¡Ganaste!</h1>
+                <p>Encontraste {numOfPairsOnDeck} pares en {numberOfPairsSelected} intentos</p>
+                <p onClick={() => {
+                    startNewGame(numOfPairsOnDeck, rickCharacters)}}>
+                        Volver a jugar
+                </p>
             </div>
         </div>
     )
 }
+
+const connectActions = {
+    startNewGame
+}
+
+const connectStore = store => ({
+    rickCharacters: store.rickCharacters,
+    numOfPairsOnDeck: store.game.numOfPairsOnDeck,
+    numberOfPairsSelected: store.game.numberOfPairsSelected
+})
+
+export default connect(connectStore, connectActions)(WonModal)
